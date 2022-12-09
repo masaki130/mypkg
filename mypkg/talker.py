b4c2a644debd99edad5101c900159482479471fb
Masaki Mitani <pasapasamasaki0@gmail.com>
2022-12-09 15:26:19 +0900

    make Class talker.py

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -6,10 +6,10 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 class Talker():
-    def __init__(self, node):
+    def __init__(self, node_ref):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
-        node.create_timer(0.5, self.cb)
+        node.create_timer(0.5, self.cb)#エラー
 
 def cb(self):
     msg = Int16()
@@ -17,11 +17,7 @@ def cb(self):
     self.pub.publish(msg)
     self.n += 1
 
-def main():
-    rclpy.init()
-    node = Node("talker")
-    talker = Talker(node)
-    rclpy.spin(node)
-
-if __name__ == '__main__':
-    main()
+rclpy.init()
+node = Node("talker")
+talker = Talker(node)#エラー
+rclpy.spin(node)
