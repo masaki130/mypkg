0c757c030f980ab4d781754a597bdede3e704397
Masaki Mitani <pasapasamasaki0@gmail.com>
2022-12-09 15:13:32 +0900

    add a command of making Class and Mesod

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 #SPDX-FileCopyrightText:2022 Masaki Mitani
 #SPDX-License Identifier;BSD-3-Clause
-
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
 class Talker():
-    def __init__(self):
+    def __init__(self, node):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
-rclpy.init()
-node = Node("talker")
-talker = Talker()
+        node.create_timer(0.5, self.cb)
 
-def cb():
+def cb(self):
     msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
+    msg.data = self.n
+    self.pub.publish(msg)
+    self.n += 1
 
-node.create_timer(0.5, cb)
-rclpy.spin(node)
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
